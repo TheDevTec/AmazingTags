f3695ee3b5c61350df110d6bab12a37dc0bc8ef5
TheDevTec <SpigotDevTec@email.cz>
2021-12-12 05:44:34 +0100

    Updated to latest TheAPI

--- src/me/devtec/amazingtags/utils/SQL.java
+++ src/me/devtec/amazingtags/utils/SQL.java
@@ -1,6 +1,5 @@
 package me.devtec.amazingtags.utils;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -52,61 +51,30 @@ public class SQL {
 	
 	public static void createTable() {
 		Loader.connection.execute("CREATE TABLE IF NOT EXISTS "+getTablePrefix()+"users (name TEXT NOT NULL, tag TEXT NOT NULL)");
-		//Loader.connection.createStatement().execute("CREATE TABLE IF NOT EXISTS "+getTablePrefix()+"users (name TEXT NOT NULL, tag TEXT NOT NULL)");
 	}
 
-	public static void selectTag(Player player, String tag) {
-		if(tag!=null) {
-			try {
-				ResultSet set = Loader.connection.query("select * from "+getTablePrefix()+"users where name='"+player.getName()+"'");
-				//ResultSet set = Loader.connection.createStatement().executeQuery("select * from "+getTablePrefix()+"users where name='"+player.getName()+"'");
-				if(set!=null && set.next()) {
-					Loader.connection.execute("update "+getTablePrefix()+"users set tag='"+tag+"' where name='"+player.getName()+"'");
-					//Loader.connection.createStatement().execute("update "+getTablePrefix()+"users set tag='"+tag+"' where name='"+player.getName()+"'");
-				}
-				else {
-					PreparedStatement preparedStmt = Loader.connection.getPreparedStatement("insert into "+getTablePrefix()+"users (name, tag)"+ " values (?, ?)");
-					//PreparedStatement preparedStmt = Loader.connection.prepareStatement("insert into "+getTablePrefix()+"users (name, tag)"+ " values (?, ?)");
-					preparedStmt.setString(1, player.getName());
-					preparedStmt.setString(2, tag);
-					preparedStmt.execute();
-				}
-			
-			} catch (Exception e) {}
-			//Bukkit.broadcastMessage("tag uložen");
-		}else {
-			Loader.connection.execute("delete from "+getTablePrefix()+"users where name='"+player.getName()+"'");
-			//Loader.connection.createStatement().execute("delete from "+getTablePrefix()+"users where name='"+player.getName()+"'");
-		}
-	}
-	/*  TODO - NOVÝ THEAPI IMPORT
 	 public static void selectTag(Player player, String tag) {
-        if(tag!=null) {
-            try {
-                ResultSet set = Loader.connection.query("select * from "+getTablePrefix()+"users where name='"+player.getName()+"'");
-                if(set!=null && set.next()) {
-                    Loader.connection.execute("update "+getTablePrefix()+"users set tag='"+tag+"' where name='"+player.getName()+"'");
-                }
-                else {
-                    Loader.connection.execute("insert into "+getTablePrefix()+"users (name, tag)"+ " values ("+player.getName()+", "+tag+")");
-                }
-            
-            } catch (Exception e) {}
-        }else {
-            Loader.connection.execute("delete from "+getTablePrefix()+"users where name='"+player.getName()+"'");
-        }
-    }
-	 */
+       if(tag!=null) {
+           try {
+               ResultSet set = Loader.connection.query("select * from "+getTablePrefix()+"users where name='"+player.getName()+"'");
+               if(set!=null && set.next()) {
+                   Loader.connection.execute("update "+getTablePrefix()+"users set tag='"+tag+"' where name='"+player.getName()+"'");
+               }
+               else {
+                   Loader.connection.execute("insert into "+getTablePrefix()+"users (name, tag)"+ " values ("+player.getName()+", "+tag+")");
+               }
+           
+           } catch (Exception e) {}
+       }else {
+           Loader.connection.execute("delete from "+getTablePrefix()+"users where name='"+player.getName()+"'");
+       }
+   }
 	
 	public static String getTag(Player player) {
 		try {
 			ResultSet rs = Loader.connection.query("select * from "+getTablePrefix()+"users where name='"+player.getName()+"'");
-			//ResultSet rs = Loader.connection.createStatement().executeQuery("select * from "+getTablePrefix()+"users where name='"+player.getName()+"'");
-		if(rs!=null && rs.next()) {
-			//while (rs.next()) {
-				return rs.getString("tag");
-			//}
-		}
+		if(rs!=null && rs.next())
+			return rs.getString("tag");
 		return null;
 		} catch (SQLException e) {
 		}
